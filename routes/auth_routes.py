50e1bfdaaa657fd1d72bdda775b34b5c6d5d189b
ehtesham925 <mohammed.ehtesham@aimtechnologies.in>
2025-08-05 15:56:26 +0530

    solving authentication

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -97,7 +97,7 @@ def login():
             return jsonify({'error': 'Account is deactivated'}), 401
         
         # Create tokens
-        access_token = create_access_token(identity=user.id)
+        access_token = create_access_token(identity=str(user.id))
         refresh_token = create_refresh_token(identity=user.id)
         
         return jsonify({
